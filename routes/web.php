2472f9023fce923d1515ab4918bd2dbdb59ccaa2
Possidius Capo <possidiusc@gmail.com>
2023-09-07 18:12:15 +0100

    premier cours

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    $user = "Possidius";
+    $array = [1,2,3];
+    // dd($user);  // die and dump
+    ddd($user, $array); // dump, die & debug
+    return "<h1>Bienvenu $user !🤠</h1>";
+});
